f61d315cb71ce7c831648c0058c63d0c0fcc3b4a
TheodoreZ <dragonsnik@yandex.ru>
2016-04-29 16:58:37 +0300

    Update MaterialDesignAsset.php

--- MaterialDesignAsset.php
+++ MaterialDesignAsset.php
@@ -1,12 +1,10 @@
-<?php 
+<?php
 namespace theodorez\material;
 use yii\web\AssetBundle;
-/**
- * 
- **/
+
 class MaterialDesignLiteAsset extends AssetBundle
 {
-	public 	$sourcePath = '@bower/material-design';
+	public $sourcePath = '@bower/material-design';
 	public $css = [
 		'material.min.css'
 	];

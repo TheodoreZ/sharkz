875ad00c83a68e20b290ffe4710e8f17325f2e5b
TheodoreZ <dragonsnik@yandex.ru>
2016-04-29 17:58:20 +0300

    Create MaterialDesignLiteAsset.php

---
+++ MaterialDesignLiteAsset.php
@@ -0,0 +1,15 @@
+<?php 
+namespace TheodoreZ\mdl\material;
+use yii\web\AssetBundle;
+
+class MaterialDesignLiteAsset extends AssetBundle
+{
+	public $sourcePath = '@bower/material-design-lite';
+	public $css = [
+		'material.min.css'
+	];
+	public $js = [
+		'material.min.js'
+	];
+}
+?>
